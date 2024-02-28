fc4ba8ee56fffd5b8d36cf7296d3f7353d9047a2
ialiev <alieff.ilya@gmail.com>
2024-02-28 23:39:32 +0700

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
-import { createApp, reactive } from 'vue';
+import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 
 const app = createApp(App);
-
-app.config.globalProperties.$editorData = reactive({
-  value: null,
-});
 
 app.use(createPinia());
 
